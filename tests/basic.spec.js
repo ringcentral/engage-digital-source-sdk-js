18c4b9552dcddb352b525ccd7e6d64e4d80261eb
ZHAO Xudong <zxdong@gmail.com>
2019-10-29 14:04:24 +0800

    Add test and CI

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -1,7 +1,9 @@
 /* eslint-env jest */
 
 import pack from '../package.json'
-// import axios from 'axios'
+import axios from 'axios'
+import _ from 'lodash'
+import { sign } from '../src/common/sign'
 
 require('dotenv').config()
 
@@ -18,8 +20,48 @@ function wait (time) {
 jest.setTimeout(99999)
 
 describe(pack.name, function () {
-  test('server runs and verify api works', async () => {
+  test('implementation.info', async () => {
     await wait(500)
-    // todo
+    let b1 = {
+      action: 'implementation.info'
+    }
+    let conf = {
+      headers: {
+        'X-SMCCSDK-SIGNATURE': sign(b1)
+      }
+    }
+    let url1 = `http://localhost:6066`
+    let res1 = await axios.post(url1, b1, conf).then(r => r.data)
+    expect('objects' in res1).toEqual(true)
+  })
+
+  test('threads.list', async () => {
+    await wait(500)
+    let b1 = {
+      action: 'threads.list'
+    }
+    let conf = {
+      headers: {
+        'X-SMCCSDK-SIGNATURE': sign(b1)
+      }
+    }
+    let url1 = `http://localhost:6066`
+    let res1 = await axios.post(url1, b1, conf).then(r => r.data)
+    expect(_.isArray(res1)).toEqual(true)
+  })
+
+  test('threads.show', async () => {
+    await wait(500)
+    let b1 = {
+      action: 'threads.show'
+    }
+    let conf = {
+      headers: {
+        'X-SMCCSDK-SIGNATURE': sign(b1)
+      }
+    }
+    let url1 = `http://localhost:6066`
+    let res1 = await axios.post(url1, b1, conf).then(r => r.data)
+    expect(res1 === '').toEqual(true)
   })
 })
